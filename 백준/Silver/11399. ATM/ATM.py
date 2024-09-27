00fa2e3f46bdbecead6361028ac5d4ab21181b05
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2024-09-27 15:33:50 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,14 +1,13 @@
 import sys
-from itertools import accumulate
 
 input = sys.stdin.readline
-
 n = int(input())
+time = list(map(int, input().split()))
+time.sort()
+ans = 0
+t = 0
+for i in range(n):
+    t += time[i]
+    ans += t
 
-time = list(map(int,input().split()))
-
-sorted_time = sorted(time)
-
-sum_time = list(accumulate(sorted_time))
-
-print(sum(sum_time))
+print(ans)
