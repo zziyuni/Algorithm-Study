a66a1e22999c34b7c58805d14e07ac3e6fd85877
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2024-11-15 16:34:34 +0900

    [level 0] Title: 외계어 사전, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,5 +1,5 @@
 function solution(spell, dic) {
-    if (dic.some((e)=>(e.length == spell.length) && spell.every((e2)=>e.includes(e2))))
+    if (dic.some((e)=>spell.every((e2)=>e.includes(e2))))
         return 1
     return 2
 }
