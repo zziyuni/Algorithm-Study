7dde029b390f29dad440fda14c6152d1841ad86b
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2024-11-16 18:58:16 +0900

    [level 0] Title: 문자열 밀기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,6 +1,6 @@
 function solution(A, B) {
     for(let i = 0; i <=B.length;i ++){  
-        if(A.split('').slice(-i,).join('')+A.split('').slice(0,-i).join('') === B)
+        if(A.slice(-i,)+A.slice(0,-i) === B)
             return i;   
     }
     return -1;
