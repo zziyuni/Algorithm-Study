fcaf6023fad952f3d9f07cffff9e56f1cd5e1067
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2025-04-14 20:57:47 +0900

    [Silver IV] Title: 빙고, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -6,49 +6,24 @@ bingo_board = list(list(map(int, input().split())) for _ in range(5))
 nums = list(list(map(int, input().split())) for _ in range(5))
 bingo = [[False] * 5 for _ in range(5)]
 
+pos = {}
+for i in range(5):
+    for j in range(5):
+        pos[bingo_board[i][j]] = (i, j)
+
 
 def check_bingo():
     cnt = 0
     for i in range(5):
-        flag = False
-        for j in range(5):
-            if not bingo[i][j]:
-                flag = True
-                break
-        if not flag:
+        if all(bingo[i]):
             cnt += 1
-    for i in range(5):
-        flag = False
-        for j in range(5):
-            if not bingo[j][i]:
-                flag = True
-                break
-        if not flag:
+        if all(bingo[j][i] for j in range(5)):
             cnt += 1
-    for i in range(5):
-        flag = False
-        if not bingo[i][i]:
-            flag = True
-            break
-    if not flag:
+    if all(bingo[i][i] for i in range(5)):
         cnt += 1
-    for i in range(5):
-        flag = False
-        if not bingo[i][4 - i]:
-            flag = True
-            break
-    if not flag:
+    if all(bingo[i][4 - i] for i in range(5)):
         cnt += 1
     return cnt
-
-
-def remove_num(n):
-    global bingo
-    for i in range(5):
-        for j in range(5):
-            if bingo_board[i][j] == n:
-                bingo[i][j] = True
-                return check_bingo()
 
 
 ans = 0
@@ -56,6 +31,8 @@ ans = 0
 for i in range(5):
     for j in range(5):
         ans += 1
-        if remove_num(nums[i][j]) >= 3:
+        x, y = pos[nums[i][j]]
+        bingo[x][y] = True
+        if check_bingo() >= 3:
             print(ans)
             exit(0)
