67b3f8b5502e8d33067fdc2fe8c488814d3c2e5f
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2025-04-19 00:26:13 +0900

    [Gold V] Title: 동전 2, Time: 348 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -2,17 +2,22 @@ import sys
 
 input = sys.stdin.readline
 
+INF = int(1e9)
 n, k = map(int, input().split())
-coin = list(int(input()) for _ in range(n))
-DP = [0 for _ in range(k + 1)]
 
-for i in coin:
-    for j in range(i, k + 1):
-        if j - i != 0 and DP[j - i] == 0:
-            continue
-        if DP[j] == 0:
-            DP[j] = DP[j - i] + 1
-            continue
-        DP[j] = min(DP[j], DP[j - i] + 1)
+DP = [INF] * (k + 1)
+coins = []
+for _ in range(n):
+    cost = int(input())
+    coins.append(cost)
 
-print(DP[k] if DP[k] != 0 else -1)
+DP[0] = 0
+for i in range(k + 1):
+    for j in coins:
+        if i - j < 0:
+            continue
+        DP[i] = min(DP[i - j] + 1, DP[i])
+if DP[k] == INF:
+    print(-1)
+else:
+    print(DP[k])
