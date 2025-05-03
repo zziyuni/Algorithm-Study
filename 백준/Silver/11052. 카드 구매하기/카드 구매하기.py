457c1726625f8a4ba5933b14d9d70b4c6d5d80f7
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2025-05-03 19:33:19 +0900

    [Silver I] Title: 카드 구매하기, Time: 168 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+price = list(map(int, input().split()))
+dp = [0] * (N + 1)
+
+for i in range(N + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + price[j - 1])
+
+print(dp[N])
