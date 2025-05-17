f9c6bf6b65201e8926f89a669232565ad1ad8c37
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2025-05-17 17:58:57 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,16 @@
+def solution(n, lost, reserve):
+    reserve_set = set(reserve) - set(lost)
+    lost_set = set(lost) - set(reserve)
+
+    for r in sorted(reserve_set):
+        if r - 1 in lost_set:
+            lost_set.remove(r - 1)
+        elif r + 1 in lost_set:
+            lost_set.remove(r + 1)
+
+    return n - len(lost_set)
+    
+            
+        
+    
+    return answer
