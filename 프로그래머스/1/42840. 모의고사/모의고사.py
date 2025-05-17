0a9b34092e8ae0fc2d8569c2ece46e5d07e791c0
jiyun yun <86959644+zziyuni@users.noreply.github.com>
2025-05-17 17:23:20 +0900

    [level 1] Title: 모의고사, Time: 2.42 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,35 +3,18 @@ def solution(answers):
     two = [2,1,2,3,2,4,2,5]
     three = [3,3,1,1,2,2,4,4,5,5]
     answer = []
-    cnt_1 = 0
-    cnt_2 = 0
-    cnt_3 = 0
+    cnts = [0,0,0]
     for i in range(len(answers)):
         if one[i%5] == answers[i]:
-            cnt_1 +=1
+            cnts[0] +=1
         if two[i%8] == answers[i]:
-            cnt_2 +=1
+            cnts[1] +=1
         if three[i%10] == answers[i]:
-            cnt_3 += 1
-    if cnt_1 > cnt_2:
-        if cnt_1 > cnt_3:
-            answer.append(1)
-        elif cnt_1 < cnt_3:
-            answer.append(3)
-        else:
-            answer= [1,3]
-    elif cnt_1 <cnt_2:
-        if cnt_2 > cnt_3:
-            answer.append(2)
-        elif cnt_2 < cnt_3:
-            answer.append(3)
-        else:
-            answer= [2,3]
-    else:
-        if cnt_1 == cnt_3:
-            answer = [1,2,3]
-        else:
-            answer =[1,2]
+            cnts[2] += 1
+    max_score = max(cnts)
+    for i in range(3):
+        if cnts[i] == max_score:
+            answer.append(i+1)
             
         
             
